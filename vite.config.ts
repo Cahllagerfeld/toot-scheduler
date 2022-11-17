8c2f16617948518c7fa09cc91d366bdddbed7a12
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2022-11-17 17:16:08 +0000

    fix: Build (#3)

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,13 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import type { UserConfig } from 'vite';
 
 const config: UserConfig = {
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	server: {
+		hmr: {
+			clientPort: process.env.HMR_HOST ? 443 : 5173,
+			host: process.env.HMR_HOST ? new URL(process.env.HMR_HOST).hostname : 'localhost'
+		}
+	}
 };
 
 export default config;
