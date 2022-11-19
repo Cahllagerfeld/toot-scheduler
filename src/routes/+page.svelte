9ffdfec7eb4c20faefe38fbe57ee4331db6d4889
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2022-11-19 16:45:48 +0000

    feat: button

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import Card from '$lib/components/atoms/card.svelte';
+	import Button from '$lib/components/atoms/button.svelte';
 </script>
 
 <Card class="mt-8">
@@ -11,3 +12,5 @@
 	<h1>Welcome to SvelteKit</h1>
 	<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 </Card>
+
+<Button class="mt-4">Click me</Button>
