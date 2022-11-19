36edb94f2842c54016189214cb94984be7ba8dc3
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2022-11-19 15:58:36 +0000

    feat: add layout & fonts

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,11 @@
-<script>
+<script lang="ts">
 	import '../app.css';
 </script>
 
-<slot />
+<div data-sveltekit-prefetch class="flex min-h-screen flex-col">
+	<div class="float-none mx-auto my-0 w-[90%] max-w-[1440px] pb-8">
+		<main>
+			<slot />
+		</main>
+	</div>
+</div>
