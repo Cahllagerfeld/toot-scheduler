b6b0864f719aa7fc3165e4f91313139c5e5c46a1
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2022-11-19 16:26:22 +0000

    feat: add card component

---
+++ src/lib/components/atoms/card.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	let className = '';
+	export { className as class };
+</script>
+
+<div class="rounded-lg bg-card p-6 shadow-default {className}">
+	<slot />
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,13 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import Card from '$lib/components/atoms/card.svelte';
+</script>
+
+<Card class="mt-8">
+	<h1>Welcome to SvelteKit</h1>
+	<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+</Card>
+
+<Card class="mt-8">
+	<h1>Welcome to SvelteKit</h1>
+	<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+</Card>
