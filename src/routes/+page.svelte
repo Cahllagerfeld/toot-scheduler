b4d92c0d25068420c54f4d45970d30c93297a57a
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2022-12-02 21:47:39 +0000

    feat: add textarea

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,9 +2,14 @@
 	import Card from '$lib/components/atoms/card.svelte';
 	import Button from '$lib/components/atoms/button.svelte';
 	import Input from '$lib/components/atoms/input.svelte';
+	import Textarea from '../lib/components/atoms/textarea.svelte';
 
 	let accessToken = '';
 	let serverURL = '';
+
+	let disableTextarea = true;
+
+	let toot = '';
 
 	const loginHandler = async () => {
 		const endpoint = `${serverURL}/api/v1/accounts/verify_credentials`;
@@ -14,34 +19,52 @@
 		if (res.ok) {
 			const data = await res.json();
 			console.log(data);
+			disableTextarea = false;
 		}
+	};
+
+	const handleSchedule = async () => {
+		console.log('handle fired');
 	};
 </script>
 
-<Card class="mt-8">
-	<form on:submit|preventDefault={loginHandler} class="flex flex-col gap-4">
-		<div>
-			<Input
-				type="password"
-				required
-				bind:value={accessToken}
-				label="Token"
-				placeholder="Your token"
-				id="access-token"
-			/>
-		</div>
-		<div>
-			<Input
-				type="text"
-				required
-				bind:value={serverURL}
-				label="Server URL"
-				placeholder="Your Server URL"
-				id="server-url"
-			/>
-		</div>
-		<div>
-			<Button type="submit">Login</Button>
-		</div>
-	</form>
-</Card>
+<div class="flex flex-col gap-8">
+	<Card class="mt-8">
+		<form on:submit|preventDefault={loginHandler} class="flex flex-col gap-4">
+			<div>
+				<Input
+					type="password"
+					required
+					bind:value={accessToken}
+					label="Token"
+					placeholder="Your token"
+					id="access-token"
+				/>
+			</div>
+			<div>
+				<Input
+					type="text"
+					required
+					bind:value={serverURL}
+					label="Server URL"
+					placeholder="Your Server URL"
+					id="server-url"
+				/>
+			</div>
+			<div>
+				<Button type="submit">Login</Button>
+			</div>
+		</form>
+	</Card>
+
+	<Card>
+		<form class="flex flex-col gap-4" on:submit|preventDefault={handleSchedule}>
+			<div>
+				<Textarea label="Your toot" required rows="6" class="resize-none" bind:value={toot} />
+			</div>
+			<div>
+				<Button disabled={disableTextarea} type="submit">Schedule</Button>
+			</div>
+		</form>
+	</Card>
+</div>
