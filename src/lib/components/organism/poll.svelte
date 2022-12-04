50a9957aa040880b29541740077386491ffaa0d1
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2022-12-04 08:30:59 +0000

    feat: init poll

---
+++ src/lib/components/organism/poll.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import Button from '$lib/components/atoms/button.svelte';
+	import Input from '../atoms/input.svelte';
+
+	let pollForm: HTMLFormElement;
+	let newOption = '';
+	let options: string[] = [];
+
+	let addNewOption = () => {
+		options = [...options, newOption];
+		newOption = '';
+		pollForm.reset();
+	};
+</script>
+
+<section>
+	<ul>
+		{#each options as option}
+			<li>
+				{option}
+			</li>
+		{/each}
+	</ul>
+	<form class="space-y-4" bind:this={pollForm} on:submit|preventDefault={addNewOption}>
+		<Input required placeholder="New Option" id="new-option" bind:value={newOption} />
+		<Button type="submit">Add Option</Button>
+	</form>
+</section>
